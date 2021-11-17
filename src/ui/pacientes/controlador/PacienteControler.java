56623b127f0dbeb942e01848e13c503d0845239c
Agustín Maio <agustinmaio.01@gmail.com>
2021-11-16 21:18:12 -0300

    [FIX] Typo

--- src/ui/pacientes/controlador/PacienteControler.java
+++ src/ui/pacientes/controlador/PacienteControler.java
@@ -4,7 +4,7 @@ import models.Sexo;
 import ui.pacientes.models.Paciente;
 
 import java.util.List;
-import ui.pacientes.models.PacientesTable
+import ui.pacientes.models.PacientesTable;
 public class PacienteControler extends Paciente {
     public PacienteControler(String nombre, int dni, String domicilio, String mail, int edad, Sexo sexo) {
         super(nombre, dni, domicilio, mail, edad, sexo);
